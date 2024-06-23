2672ec4299cca74179024bd32fe13e4441baf5b0
ryoiida0306 <ryo.iida0306@gmail.com>
2024-06-23 13:19:44 +0900

    abc359

---
+++ abc359/b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+#include <cmath>
+using namespace atcoder;
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,n) for(int i = 1; i <= (n); ++i)
+#define rrep(i,n) for(int i = (n)-1; i >= 0 ; --i)
+#define rrep1(i,n) for(int i = (n); i > 0; --i)
+// #define all(a) (a).begin, (a).end()
+using mint = modint998244353;
+using mint2 = modint1000000007;
+using ll = long long;
+using P = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+using vd = vector<double>;
+using vvd = vector<vector<double>>;
+using vc = vector<char>;
+using vvc = vector<vector<char>>;
+using vs = vector<string>;
+using vsi = vector<set<int>>;
+using vsl = vector<set<ll>>;
+using vb = vector<bool>;
+using vvb = vector<vector<bool>>;
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+int INF = 1001001001;
+ll LINF = 1001001001001001001;
+using mytpl = tuple<ll,int,int>;
+
+int main(){
+  int n;
+  cin >> n;
+  vi a(2*n);
+  rep(i,2*n) cin >> a[i];
+  int cnt = 0;
+  rep(i,2*n-2){
+    if(a[i+2] == a[i]){
+      cnt++;
+    }
+  }
+  cout << cnt << endl;
+  return 0;
+}
